7b24c8e7af510b28922ce0492bed30ee4d48f952
Hendi Santika <hendisantika@gmail.com>
2020-01-03 06:19:27 +0700

    #6 - Add exportCSV API in UserController

--- src/main/java/com/hendisantika/springbootexportdatacsv/controller/UserController.java
+++ src/main/java/com/hendisantika/springbootexportdatacsv/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.hendisantika.springbootexportdatacsv.controller;
 
+import com.hendisantika.springbootexportdatacsv.data.User;
 import com.hendisantika.springbootexportdatacsv.data.UserService;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,5 +27,26 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/export-users")
+    public void exportCSV(HttpServletResponse response) throws Exception {
+        //set file name and content type
+        String filename = "users.csv";
+
+        response.setContentType("text/csv");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + filename + "\"");
+
+        //create a csv writer
+        StatefulBeanToCsv<User> writer = new StatefulBeanToCsvBuilder<User>(response.getWriter())
+                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+                .withOrderedResults(false)
+                .build();
+
+        //write all users to csv file
+        writer.write(userService.listUsers());
+
     }
 }
